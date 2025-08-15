84f7f1f85946670d9bcb57050a8bd2244fc0a8f8
Cristian <cristiancamilolara@hotmail.com>
2025-08-15 12:20:02 -0500

    update

--- clases.dart
+++ clases.dart
@@ -2,8 +2,9 @@ class PersonaActividad {
   String nombre;
   int edad;
   double altura;
+  double salario;
 
-  PersonaActividad(this.nombre, this.edad, this.altura); //valor para las variables
+  PersonaActividad(this.nombre, this.edad, this.altura, this.salario); //valor para las variables
   //Metodos u operaciones
   //Funcion para saludar
   void presentarse(){ // funcion que no recibe valores
@@ -14,20 +15,28 @@ class PersonaActividad {
     if (this.edad >= 18 ) {
       print("soy mayor de edad");
     } else {
-      print("no es mayor");
+      print("no soy mayor");
+    }
+  }
+
+  void Salario(){
+    if (this.salario >1500000) {
+      print('subsidio de alimentacion es ${this.salario*0.023}');
+      print('valor retefuente es ${this.salario*0.08}');
+      print('Salario total es ${(this.salario*0.08)+(this.salario*0.023)+this.salario}');
     }
   }
 } //Fin de clase personaActividad
 
 void main(){
-  PersonaActividad persona = PersonaActividad('Carlos', 34, 1.75);
+  PersonaActividad persona = PersonaActividad('Carlos', 34, 1.75, 1400000);
   // instanciar o crear el objeto
   persona.presentarse();
-  print(persona.altura);
+  print('mi estatura es ${persona.altura}');
   
-
-  PersonaActividad persona2 = PersonaActividad("Ana maria", 21, 1.60);
+  PersonaActividad persona2 = PersonaActividad("Ana maria", 21, 1.60, 1700000);
   persona2.presentarse();
   persona2.menor();
+  persona2.Salario();
 
 }
